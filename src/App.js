2fa67bb2f6b188cf6797eb678d5705ebef18c2e2
Antonio Krsoski Windows <antoniok.inquiries@gmail.com>
2023-10-01 16:55:15 +0200

    api test completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [res, setRes] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => TestAPI(res, setRes)}>Click here for margarita logs</button>
     </div>
   );
 }
+
+async function TestAPI(res, setRes) {
+
+  await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(response => response.json()).then(data => setRes(data.drinks));
+
+  console.log(res);
+  console.log(res[0].strDrink);
+  console.log(res[0].strInstructions);
+  console.log(res[0].strDrinkThumb);
+  console.log(res[0].strIngredient1);
+  console.log(res[0].strIngredient2);
+  console.log(res[0].strIngredient3);
+  console.log(res[0].strIngredient4);
+
+  return(
+    <>
+      <p>{res[0].strDrink}</p>
+    </>
+  );
+}
+
+
+
 
 export default App;
