5f3fb3e866bcc540d912af4236c3614408b5e9f0
andreroxhage <an8603ro-s@student.lu.se>
2023-10-02 16:23:50 +0200

    CocktailList ui

--- src/CocktailList.js
+++ src/CocktailList.js
@@ -10,19 +10,52 @@ export default function CocktailList() {
 		{
 			title: 'Test 1',
 			description: 'bläää',
-			src: 'bla',
+			src: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg/preview',
 		},
-		{title: 'Test 2', description: 'bläää', src: 'bla'},
-		{title: 'Test 3', description: 'bläää', src: 'bla'},
+		{
+			title: 'Test 2',
+			description: 'bläää',
+			src: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg/preview',
+		},
+		{
+			title: 'Test 3',
+			description: 'bläää',
+			src: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg/preview',
+		},
+		{
+			title: 'Test 4',
+			description: 'bläää',
+			src: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg/preview',
+		},
+		{
+			title: 'Test 5',
+			description: 'bläää',
+			src: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg/preview',
+		},
+		{
+			title: 'Test 6',
+			description: 'bläää',
+			src: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg/preview',
+		},
 	];
 	return (
-		<Container className='pt-4'>
-			<div className='d-flex flex-row justify-content-center'>
+		<Container className='pt-4 mt-4'>
+			<div className='d-flex flex-wrap flex-row w-100'>
 				{cards.map((card) => (
-					<Card className='m-2 w-33' key={card.title} style={{width: '18rem'}}>
+					<Card className='m-2 w-50' key={card.title} style={{width: '18rem'}}>
 						<Card.Img src={card.src} variant='top'></Card.Img>
-						<Card.Title>{card.title}</Card.Title>
-						<Card.Text>{card.description}</Card.Text>
+						<Card.Title
+							className='mt-3'
+							style={{textAlign: 'left', paddingLeft: '10px'}}
+						>
+							{card.title}
+						</Card.Title>
+						<Card.Text
+							className='mt-3'
+							style={{textAlign: 'left', paddingLeft: '10px'}}
+						>
+							{card.description}
+						</Card.Text>
 					</Card>
 				))}
 			</div>
