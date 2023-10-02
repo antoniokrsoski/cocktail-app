36b7aff68bddb6257cd5bb182b58c8c406dbb255
Antonio Krsoski <antonio.krsoski.0473@student.lu.se>
2023-10-02 13:35:17 +0200

    new api fetch done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
   const [res, setRes] = useState();
+  useEffect(() => {
+    let shouldFetch = true;
+    if (shouldFetch) {
+      shouldFetch = false;
+      let res = fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then(response => response.json())
+        .then(data => setRes(data.drinks));
+    }
+    return () => { shouldFetch = false; }
+  }, []);
 
   return (
     <div className="App">
-        <button onClick={() => TestAPI(res, setRes)}>Click here for margarita logs</button>
+      {res ? <img src={res[0].strDrinkThumb + "/preview"} alt="margarita" /> : <div>loading...</div>}
+      {res ? <div>{JSON.stringify(res[0].strInstructions)}</div> : <div>loading... 2</div>}
     </div>
   );
 }
 
-async function TestAPI(res, setRes) {
 
-  await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(response => response.json()).then(data => setRes(data.drinks));
-
-  console.log(res);
-  console.log(res[0].strDrink);
-  console.log(res[0].strInstructions);
-  console.log(res[0].strDrinkThumb);
-  console.log(res[0].strIngredient1);
-  console.log(res[0].strIngredient2);
-  console.log(res[0].strIngredient3);
-  console.log(res[0].strIngredient4);
-
-  return(
-    <>
-      <p>{res[0].strDrink}</p>
-    </>
-  );
-}
 
 
 
