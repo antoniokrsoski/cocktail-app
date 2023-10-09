642e3b09f5abb5469a45a418fcac8c257aa57172
Your Name <lisa.balke@yahoo.se>
2023-10-09 16:39:12 +0200

    single drink page done

--- src/pages/singledrink/SingleDrink.js
+++ src/pages/singledrink/SingleDrink.js
@@ -1,23 +1,50 @@
 import React from "react";
 import { useLoaderData, useParams, useNavigate } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
+import './SingleDrink.css'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from "react-bootstrap/esm/Container";
 
 export default function SingleDrink(props) {
     const drink = useLoaderData();
     console.log("drink", drink);
+
+    const arr = [];
+    for(let n=1; n<16; n++){
+        let ind='strIngredient'+n;
+        let ind2= 'strMeasure'+n;
+    
+        arr.push(drink[ind] ? drink[ind2] + drink[ind] : "");
+    }
+
     return (
         // todo, fix the UI for the single drink card :)
-        <div className='d-flex justify-content-center'>
-            <Card style={{ width: '28rem'}}>
-                <Card.Img variant="right" src={drink.strDrinkThumb} />
+        <Container fluid="lg" className='d-flex justify-content-center'>
+            <Card className='bigcard '>
+                <Row>
+                <Col>
                 <Card.Body>
-                    <Card.Title>{drink.strDrink}</Card.Title>
-                    <Card.Text>
-                        {/*  */}
+                    <Card.Title className="bigcard">{drink.strDrink}</Card.Title>
+                    <Card.Text className="bigcard">
+                        {drink.strInstructions}
                     </Card.Text>
                 </Card.Body>
+                <ListGroup className="bigcard" variant='flush'>
+                    <ListGroup.Item>Ingredients</ListGroup.Item>
+                    
+                    {arr.map((ingr)=> ingr ? <ListGroup.Item>{ingr}{}</ListGroup.Item> : <></>)}
+                    
+                </ListGroup>
+                </Col>
+
+                <Col >
+                <Card.Img src={drink.strDrinkThumb} className="w-100 h-100 bg-contain" ></Card.Img>
+                </Col>
+                </Row>
             </Card>
-        </div>
+        </Container>
     );
 }
 
